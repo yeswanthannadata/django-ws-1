903058ce83e7b358143f65419476df765af1c83d
Yeshu <yeswanth@flutura.com>
2022-03-29 18:52:06 +0530

    first cut changes

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,8 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog_details.html'
