34e6018f360786c342fe7c3434ad63c2a4b429ca
Yeshu <yeswanth@flutura.com>
2022-03-30 12:09:39 +0530

    changes sepecifc to blog CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
     path('post/<int:pk>', BlogDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/update', BlogUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete', BlogDeleteView.as_view(), name='post_delete'),
+    path('post/new', BlogCreateView.as_view(), name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 # Create your views here.
 from .models import Post
@@ -13,3 +14,21 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog_details.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'blog_new.html'
+    fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog_update.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog_delete.html'
+    success_url = reverse_lazy('home')
